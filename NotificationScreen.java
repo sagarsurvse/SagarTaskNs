5fdea735997233d87292bee846e4e60091d5fa13
Sagar Survase(Android Devoloper) <49334643+sagarsurvse@users.noreply.github.com>
2020-05-12 09:57:42 -0700

    Add files via upload

---
+++ NotificationScreen.java
@@ -0,0 +1,13 @@
+package com.example.liew.ideliveryserver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotificationScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_screen);
+    }
+}
